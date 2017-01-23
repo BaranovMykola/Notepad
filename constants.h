2eb053180e5a89e752ceba69408c8f0c76a9c769
BaranovMykola <mapsg32@gmail.com>
2017-01-23 15:42:09 +0200

    Removed unused constant.

--- constants.h
+++ constants.h
@@ -14,5 +14,4 @@ const int IntMaxLenght = 10;
 const int DefaultFontSize = 9;
 const QString ConfigPath = "config"; // dir in one deep with exe-file.
 const QString ConfigNameFile = "config.json";
-const QString IconName = "Notepad.ico";
 #endif // CONSTANTS_H
